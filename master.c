28cad0a17708356fe5bd15b474a9f2b2ca86c516
F94036089 <f94036089@ncku.edu.tw>
2017-11-26 23:30:52 +0800

    Add passing parameters

--- master.c
+++ master.c
@@ -1,19 +1,51 @@
 #include "master.h"
-
+#include "stdlib.h"
+#include "unistd.h"
 int main(int argc, char **argv)
 {
-	/*
-	 * write something or nothing
-	 */
-}
+	char ch;
+    char* word;
+    char* directory;
+    int K=1;
+    while ((ch = getopt(argc, argv, "q:d:s:")) != EOF) {
+        switch (ch) {
+        case 'q':
+            printf("-q %s\r\n", optarg);
+            word=optarg;
+            break;
+        case 'd':
+            printf("-d %s\r\n", optarg);
+            directory=optarg;
+			break;
+		case 's':
+            printf("-s %s\r\n", optarg);
+            K=atoi(optarg);
+			break;
+        default:
+            fprintf(stderr, "Unknown option: '%s'\n", optarg);
+            return 1;
+        }
 
+    }
+    printf("%s\n",word);
+    printf("%s\n",directory);
+	printf("%d\n",K);
+	struct mail_t *mail;
+	mail.query_word=word;
+	mail.file_path=directory;
+	int sysfs_fd = open("/sys/kernel/hw2/mailbox", O_WRONLY|O_CREAT, 0700);
+	send_to_fd(sysfs_fd, &mail);
+	read_from_fd(sysfs_fd, &mail);
+	close(f);
+    return 0;
+}
 int send_to_fd(int sysfs_fd, struct mail_t *mail)
 {
 	/*
 	 * write something or nothing
 	 */
 
-	int ret_val = write(sysfs_fd, ...);
+	int ret_val = write(sysfs_fd, &mail, sizeof(struct mail_t));
 	if (ret_val == ERR_FULL) {
 		/*
 		 * write something or nothing
@@ -28,14 +60,13 @@ int send_to_fd(int sysfs_fd, struct mail_t *mail)
 	 * write something or nothing
 	 */
 }
-
 int receive_from_fd(int sysfs_fd, struct mail_t *mail)
 {
 	/*
 	 * write something or nothing
 	 */
 
-	int ret_val = read(sysfs_fd, ...);
+	int ret_val = read(sysfs_fd, &mail, sizeof(struct mail_t));
 	if (ret_val == ERR_EMPTY) {
 		/*
 		 * write something or nothing
